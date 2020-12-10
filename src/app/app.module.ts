86d1d5fb28f149ac16b9c6ba5f8a5b49dd69a998
Rodrigo <rodrigo_moreira84@hotmail.com>
2020-12-10 18:40:23 -0300

    adicionada pesquisa de lancamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@ import { NgModule } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
 
 import { AppComponent } from './app.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent
   ],
   imports: [
     BrowserModule,
     InputTextModule,
     ButtonModule,
-    TableModule ,
-
+    TableModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
